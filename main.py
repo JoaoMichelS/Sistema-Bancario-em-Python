86d6eb79eefddb517ace90a88d2296283bf8584f
João Victor Michel Silva <t_joao.silva@prefeitura.local>
2023-07-11 12:08:19 -0300

    Update informativo desafio

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 '''
-Desafio
+Desafio v1.0
 
 Fomos contratados por um grande banco para desenvolver o seu novo sistema. Esse banco deseja modernizar 
 suas operações e para isso escolheu a linguagem Python. Para a primeira versão do sistema devemos implementar 
@@ -23,6 +23,55 @@ Essa operação deve listar todos os depósitos e saques realizados na conta. No
 o saldo atual da conta.
 Os valores devem ser exibidos utilizando o formato R$ xxx.xx, exemplo:
 1500.45 = R$ 1500.45
+
+Desafio v2.0
+
+Precisamos deixar nosso código mais modularizado, para isso vamos criar funções para as operações existentes: sacar, 
+depositar e visualizar histórico. Além disso, para a versão 2 do nosso sistema precisamos criar duas novas funções: 
+criar usuário (cliente do banco) e criar conta corrente (vincular com usuário).
+
+Separação em funções 
+
+Devemos criar funções para todas as operações do sistema. Para exercitar tudo o que aprendemos neste módulo, cada 
+função vai ter uma regra na passagem de argumentos. O retorno e a forma como serão chamadas, pode ser definida por
+você da forma que acha melhor.
+
+Saque 
+
+A função saque deve receber os argumentos apenas por nome (keyword only). Sugestão de argumentos: saldo, valor, 
+extrato, limite, numero_saques, limite_saques. Sugestão de retorno: saldo e extrato.
+
+Depósito
+
+A função depósito deve receber os argumentos apenas por posição (positional only). Sugestão de argumentos: saldo, 
+valor, extrato. Sugestão de retorno: saldo e extrato.
+
+Extrato
+
+Afunção extrato deve receber os argumentos por posição e nome (positional only e keyword only). Argumentos posicionais:
+saldo, argumentos nomeados: extrato.
+
+Novas Funções
+
+Precisamos criar duas funções: criar usuários e criar conta corrente. Fique a vontade para adicionar mais funções, 
+exemplo: listar contas.
+
+Criar Usuário (cliente)
+
+O programa deve armazenar os usuários em uma lista, um usuário é composto por: nome, data de nascimento, cpf e
+endereço. O endereço é uma string com o formato: logradouro, nro - bairro - cidade / sigla estado. Deve ser armazenado
+somente os números do CPF. Não podemos cadastrar 2 usuários com o mesmo CPF.
+
+Criar Conta Corrente
+
+O programa deve armazenar contas em uma lista, uma conta é composta por: agência, número da conta e usuário. O número
+da conta é sequencial, iniciando em 1. O número da agência é fixo: "0001". O usuário pode ter mais de uma conta, mas
+uma conta pertence a somente um usuário.
+
+Dica 
+
+Para vincular um usuário a uma conta, filtre a lista de usuários buscando o número do CPF informado para cada usuário 
+da lista.
 '''
 
 import os
